bea98f5926bea7e3959f06f22c610f0278875cbf
ArtikbaevaBarno <142983980+SharipovUmid@users.noreply.github.com>
2023-08-23 18:00:23 +0500

    JS 3-dars
    26-guruh o'quvchisi

---
+++ js/main.js
@@ -0,0 +1,33 @@
+/* Math in JS */
+let round = Math.round(8.6);       /* Yaxlidlash-1 */
+let pow = Math.pow(4,3);         /* Darajasini hisoblash */
+let sqrt = Math.sqrt(81);          /* Ildizini topish */
+let abs = Math.abs(-12.8);         /* Absolut sonni aniqlash */
+let ceil = Math.ceil(-49.1);       /* Yaxlidlash-2 */
+let floor = Math.floor(49.9);      /* Yaxlidlash-3 */
+let trunc = Math.trunc(49.9);      /* Yaxlidlash-4 */
+let min = Math.min(25,17,122,779,31,47);      /* Yaxlidlash-4 */
+let max = Math.max(25,17,122,779,31,47);      /* Yaxlidlash-4 */
+
+document.write(max)
+
+/* DOM and styles in JS */
+
+/*let byTag = document.getElementsByTagName("h1")*/
+/*let byId = document.getElementById("idNomi")*/
+/*let byClass = document.getElementsByClassName("lorem")*/
+/*let bySelector = document.querySelector("#idNomi")*/
+/*let bySelectorAll = document.querySelectorAll(".lorem")*/
+/*let byAll = document.getElementsByTagName("h1").namedItem("idNomi");*/
+
+/*byAll.style.color = "yellow";*/
+/*byAll.style.backgroundColor = "blue";*/
+/*byAll.style.width = "300px";*/
+/*byAll.style.height = "250px";*/
+/*byAll.style.fontSize = "45px";*/
+/*byAll.style.justifyContent = "center";*/
+/*byAll.style.padding = "90px";*/
+/*byAll.style.display = "flex";*/
+/*byAll.style.alignItems = "center";*/
+
+/*console.log(byAll);*/
